177b4be6771de6ab116148ee5fa729caec6ed427
hoang92hh <hoang92hongha@gmail.com>
2024-05-18 22:54:24 +0700

    add clent-id for web-app

--- web-app/src/configurations/configuration.js
+++ web-app/src/configurations/configuration.js
@@ -1,5 +1,5 @@
 export const OAuthConfig = {
-  clientId: "<clientId>",
+  clientId: "999489656347-f9uos6jfnkh54qh51pgd1266k17cpltf.apps.googleusercontent.com",
   redirectUri: "http://localhost:3000/authenticate",
   authUri: "https://accounts.google.com/o/oauth2/auth",
 };
